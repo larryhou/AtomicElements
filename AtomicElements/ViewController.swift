067d2f633b799e2fd22bc2fe083e8daf16e6021c
larryhou <larryhou@foxmail.com>
2014-10-26 12:16:49 +0800

    Update code format

--- AtomicElements/ViewController.swift
+++ AtomicElements/ViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
